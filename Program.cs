9b1ea73cb16a8abbd8bb2c5d66cc1f27ed5a8963
Erik Hvattum <erik.hvattum@gmail.com>
2017-03-10 16:19:13 +0100

    Now works for .csproj instead

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 using GitVersion;
 using GitVersion.Helpers;
-using Newtonsoft.Json.Linq;
 
 namespace ConsoleApplication
 {
@@ -10,22 +11,36 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Logger.SetLoggers(
-                Console.WriteLine,
-                Console.WriteLine,
-                Console.WriteLine);
+            Logger.SetLoggers(Console.WriteLine, Console.WriteLine, Console.WriteLine);
 
             var fs = new FileSystem();
             var gv = new GitVersion.ExecuteCore(fs);
             var auth = new GitVersion.Authentication();
-
             var version = gv.ExecuteGitVersion(null, null, auth, null, false, ".", null);
-            Console.WriteLine($"Setting version: {version.LegacySemVerPadded}");
 
-            var filename = "project.json";
-            var projectJson = JObject.Parse(File.ReadAllText(filename));
-            projectJson["version"] = version.LegacySemVerPadded;
-            File.WriteAllText(filename, projectJson.ToString());
+            var file = GetFile();
+            if (file == null)
+            {
+                Console.WriteLine("Found no file");
+                return;
+            }
+            Console.WriteLine($"Setting version: {version.NuGetVersion}");
+            XDocument doc = XDocument.Load(file);
+            var propertyGroup = doc.Descendants("PropertyGroup").SingleOrDefault();
+            var versionAttribute = propertyGroup.Descendants("VersionPrefix").SingleOrDefault() as XElement;
+            if (versionAttribute != null)
+                versionAttribute.SetValue(version.NuGetVersion);
+            else propertyGroup.Add(new XElement("VersionPrefix", version.NuGetVersion));
+
+            using (FileStream stream = new FileStream(file, FileMode.Open, FileAccess.Write))
+            {
+                doc.Save(stream);
+            }
+        }
+
+        private static string GetFile()
+        {
+            return Directory.EnumerateFiles(".").FirstOrDefault(f => f.EndsWith(".csproj"));
         }
     }
 }
